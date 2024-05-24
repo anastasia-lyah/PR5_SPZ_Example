4ee42e2e9ef051ac057f14a6544d5027e384b106
Anastasia Lyah <lyahanastasya@gmail.com>
2024-05-24 10:54:01 +0300

    Добавьте файлы проекта.

---
+++ PR5_SPZ_Example/ClassesLib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR5_SPZ_Example
+{
+    public class Town
+    {
+        public string Name; 
+        public string Country; 
+        public string Region;
+        public int Population; 
+        public double YearIncome; 
+        public double Square; 
+        public bool HasPort;
+        public bool HasAirport;
+        public double yearIncomePerInhabitant
+        {
+            get
+            {
+                return GetYearIncomePerInhabitant();
+            }
+        }
+        public double GetYearIncomePerInhabitant()
+        {
+            return YearIncome / Population;
+        }
+    }
+}
